cec0e74d7949f2aa787f1b6d33ab65da075f2b69
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-02-16 15:41:55 +0700

    Add clear button to reset grid color

--- eas.js
+++ eas.js
@@ -1,16 +1,23 @@
 let sides = 16;
 
 const container = document.querySelector('#container');
+const grid = container.children;
 
 for (let i = 0; i < (sides * sides); i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.style.cssText = 'background: white; border: 1px solid red';
     
     square.addEventListener('mouseover', (e) => {
-        e.target.style.background = 'green';
+        e.target.classList.add('active');
     });
 
     container.appendChild(square);
 }
 
+const clear = document.querySelector('#clear');
+
+clear.addEventListener('click', () => {
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].classList.remove('active');
+    }
+});
