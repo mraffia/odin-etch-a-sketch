93810fb7fb8cef69d539d20ae7bf27c7d9aa5606
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-02-16 15:06:40 +0700

    Add hover effect thath chages color to grid

--- eas.js
+++ eas.js
@@ -1,14 +1,16 @@
 let sides = 16;
 
-function generateGrid() {
-    const container = document.querySelector('#container');
+const container = document.querySelector('#container');
 
-    for (let i = 0; i < (sides * sides); i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.style.cssText = 'background: yellow; border: 1px solid red';
-        container.appendChild(square);
-    }
+for (let i = 0; i < (sides * sides); i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.cssText = 'background: white; border: 1px solid red';
+    
+    square.addEventListener('mouseover', (e) => {
+        e.target.style.background = 'green';
+    });
+
+    container.appendChild(square);
 }
 
-generateGrid();
