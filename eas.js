2ee3af21ede5f5e5aaa0ecaba5c2672f1f58f485
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-02-16 21:46:46 +0700

    Fix clear button to erase entire grid and generate a new size based on user prompt

--- eas.js
+++ eas.js
@@ -3,21 +3,44 @@ let sides = 16;
 const container = document.querySelector('#container');
 const grid = container.children;
 
-for (let i = 0; i < (sides * sides); i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    
-    square.addEventListener('mouseover', (e) => {
-        e.target.classList.add('active');
-    });
+function generateGrid(sides) {
+    for (let i = 0; i < (sides * sides); i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
 
-    container.appendChild(square);
+        let squarePadding = (500 / sides) / 2;
+        square.style.cssText = `padding : ${squarePadding}px`;
+        
+        square.addEventListener('mouseover', (e) => {
+            e.target.classList.add('active');
+        });
+    
+        container.appendChild(square);
+    }
 }
+
+generateGrid(sides);
 
 const clear = document.querySelector('#clear');
 
 clear.addEventListener('click', () => {
-    for (let i = 0; i < grid.length; i++) {
-        grid[i].classList.remove('active');
+    
+    // Source: https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
+
+    let newSides = prompt("Enter number of squares per side for the new grid (maximum 100, default is 16)", "16");
+    
+    if (newSides != null) {
+        if (parseInt(newSides) == NaN) {
+            newSides = 16;
+        } else {
+            newSides = parseInt(newSides);
+        }
+    } else {
+        newSides = 16;
+    }
+
+    generateGrid(newSides);
 });
