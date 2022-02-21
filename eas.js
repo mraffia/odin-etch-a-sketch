f04cb410ae0db4c97135acd11d9c3af31d514a02
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-02-22 00:40:19 +0700

    Add black and rainbow color trail button function

--- eas.js
+++ eas.js
@@ -1,12 +1,9 @@
-let defaultSides = 16;
-
-const container = document.querySelector('#container');
-const grid = container.children;
+const gridContainer = document.querySelector('#grid-container');
+const grid = gridContainer.children;
 const gridInfo = document.querySelector('.grid-info');
+const trailInfo = document.querySelector('.trail-info');
 
-// Source: https://css-tricks.com/snippets/javascript/random-hex-color/
-// Random color generator in hex form
-let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+let defaultSides = 16;
 
 function generateGrid(sides) {
     for (let i = 0; i < (sides * sides); i++) {
@@ -14,26 +11,51 @@ function generateGrid(sides) {
         square.classList.add('square');
 
         let squarePadding = (400 / sides) / 2;
-        square.style.cssText = `padding : ${squarePadding}px`;
+        square.style.cssText = `padding: ${squarePadding}px`;
         
         square.addEventListener('mouseover', (e) => {
-            e.target.classList.add('active');
+            e.target.style.cssText += `background-color: black`;
         });
 
         gridInfo.textContent = `Grid size: ${sides}x${sides} square blocks`;
     
-        container.appendChild(square);
+        gridContainer.appendChild(square);
     }
 }
 
 generateGrid(defaultSides);
 
+const black = document.querySelector('#black');
+black.addEventListener('click', () => {
+    for (let i = 0; i < grid.length - 1; i++) {
+        grid[i].addEventListener('mouseover', (e) => {
+            e.target.style.cssText += `background-color: black`;
+        });
+    }
+    trailInfo.textContent = `Trail color: Black`;
+});
+
+const rainbow = document.querySelector('#rainbow');
+rainbow.addEventListener('click', () => {
+    for (let i = 0; i < grid.length - 1; i++) {
+        grid[i].addEventListener('mouseover', (e) => {
+            // Source: https://css-tricks.com/snippets/javascript/random-hex-color/
+            // Random color generator in hex form
+            let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+
+            e.target.style.cssText += `background-color: ${randomColor}`;
+        });
+    }
+    trailInfo.textContent = `Trail color: Rainbow`;
+});
+
 const reset = document.querySelector('#reset');
 reset.addEventListener('click', () => {
     
     // Source: https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
-    while (container.firstChild) {
-        container.removeChild(container.lastChild);
+    // Remove all child elements of a DOM node
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
     }
 
     let newSides = prompt("Enter number of squares per side for the new grid (maximum 100, default is 16)", "16");
